22ad323f150c73182ec8daf9293901ad3c3a506d
marcornc <130748589+marcornc@users.noreply.github.com>
2024-03-05 19:22:52 +0000

    responsive: hidden fix

--- src/components/Rainbow_bg.tsx
+++ src/components/Rainbow_bg.tsx
@@ -4,17 +4,17 @@ export default function RainbowBg() {
   return (
     <>
       {/* left */}
-      <div className="hidden sm:visible">
-        <div className="hidden sm:visible sm:rainbow_pink sm:top-0"></div>
-        <div className="hidden sm:visible sm:rainbow_green sm:top-[45rem]"></div>
-        <div className="hidden sm:visible sm:rainbow_pink sm:top-[85rem]"></div>
+      <div className="hidden sm:block">
+        <div className=" rainbow_pink top-0"></div>
+        <div className=" rainbow_green top-[45rem]"></div>
+        <div className=" rainbow_pink top-[85rem]"></div>
       </div>
 
       {/* right */}
-      <div className="absolute right-0">
+      <div className="sm:absolute sm:right-0">
         <div className="rainbow_green sm:top-0"></div>
-        <div className="top-[20rem] rainbow_pink sm:top-[40rem]"></div>
-        <div className="top-[40rem] rainbow_green sm:top-[80rem]"></div>
+        <div className="rainbow_pink top-[20rem]  sm:top-[40rem]"></div>
+        <div className="rainbow_green top-[40rem] rainbow_green sm:top-[80rem]"></div>
       </div>
     </>
   );
