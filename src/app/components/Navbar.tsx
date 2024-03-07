13fd4983a551a3cb58ec600d133ec999d1d06908
Marika <ferrari.marika@gmail.com>
2024-03-07 10:49:04 +0000

    Alternative download button

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -44,9 +44,23 @@ const Navbar = () => {
           href="#"
           className="inline-block text-sm px-4 py-2 border rounded shadow-xl bg-[#E55982] text-slate-200 border-slate-700 font-medium mt-4 lg:mt-0"
         >
-          Download the app
+          <button>Download the app</button>
         </a>
       </div>
+      {/* Download the app alternative */}
+      {/* <div className="w-40">
+        <a
+          href="#"
+          className="inline-block text-sm px-4 py-2 border rounded shadow-xl bg-[#E55982] text-slate-200 border-slate-700 font-medium mt-4 lg:mt-0"
+        >
+          <button className="rounded inline-flex items-center gap-2">
+            <span>Get the app</span>
+            <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+            </svg>
+          </button>
+        </a>
+      </div> */}
     </nav>
   );
 };
