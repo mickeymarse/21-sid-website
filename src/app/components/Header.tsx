f0ce8b8db8f63afb0bd70b411b026d59525a8efc
Marika <135352868+marikaferrari@users.noreply.github.com>
2024-03-08 11:47:20 +0000

    Adjustment to align better with navbar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 const Header = () => {
   return (
-    <div className="flex flex-col items-center justify-center mt-12">
+    <div className="flex flex-col items-center justify-center">
       <Avatar className="w-20 h-20 my-6">
         <AvatarImage src="https://github.com/shadcn.png" />
         <AvatarFallback>21 SID</AvatarFallback>
