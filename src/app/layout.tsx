1a8d2bb62fe95ad6fc4e80bc869ca95cf12b9866
Marika <ferrari.marika@gmail.com>
2024-04-09 15:33:39 +0100

    Import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,19 @@
-
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-import Navbar from './components/Navbar';
-
 import Header from './components/Header';
 
-import Footer from "@/components/Footer";
+import Footer from '@/components/Footer';
 
-import RainbowBg from "@/components/Rainbow_bg";
-
-
+import RainbowBg from '@/components/Rainbow_bg';
+import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-
   title: '21 SID',
   description: '21 SID website',
-
 };
 
 export default function RootLayout({
@@ -31,7 +25,7 @@ export default function RootLayout({
     <html lang="en" className="!scroll-smooth">
       <body className="bg-gray-50 text-gray-950">
         <RainbowBg />
-        <NavBar />
+        <Navbar />
         <Header />
 
         {children}
