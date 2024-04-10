24d74a5124995fe6e6515763e785cf9515b550c0
mickeymarse <mmserrapica@tutanota.com>
2024-04-10 10:21:22 +0100

    Fixed extra componentss folder issue. And added company logo.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,12 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-import Header from './components/Header';
+import Header from '../components/Header';
 
 import Footer from '@/components/Footer';
 
 import RainbowBg from '@/components/Rainbow_bg';
-import Navbar from './components/Navbar';
+import Navbar from '../components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="!scroll-smooth">
-      <body className="bg-gray-50 text-gray-950">
+    <html lang='en' className='!scroll-smooth'>
+      <body className='bg-gray-50 text-gray-950'>
         <RainbowBg />
         <Navbar />
         <Header />
