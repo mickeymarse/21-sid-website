98ac31cb9b362c8c9d525549a87bf13a49b96e9b
marcornc <130748589+marcornc@users.noreply.github.com>
2024-06-08 20:10:51 +0100

    bigger text size for navbar

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -11,7 +11,7 @@ const NavigationButton: React.FC<NavigationButton> = ({ href, page }) => {
     <div>
       <Link
         href={href}
-        className="block hover:no-underline mt-4 lg:inline-block lg:mt-0 mr-6 text-l px-4 py-2 rounded-xl text-slate-900 font-medium hover:bg-pink_sid/60 shadow-m hover:shadow-xl transition transform hover:-translate-y-0.5 motion-reduce:transition-none motion-reduce:hover:transform-none bg-[#f2f2f2] "
+        className="block hover:no-underline mt-4 lg:inline-block lg:mt-0 mr-6 text-[1.2rem] font-normal px-4 py-2 rounded-xl text-slate-900 font-medium hover:bg-pink_sid/60 shadow-m hover:shadow-xl transition transform hover:-translate-y-0.5 motion-reduce:transition-none motion-reduce:hover:transform-none bg-[#f2f2f2] "
       >
         {page}
       </Link>
