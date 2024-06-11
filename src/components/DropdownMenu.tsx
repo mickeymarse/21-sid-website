fcfb01252414ce9599494a38b12ff1b4ec50ae18
marcornc <130748589+marcornc@users.noreply.github.com>
2024-06-11 16:43:47 +0100

    test

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -12,7 +12,6 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-
 // This is the dropdown menu that appears once clicking the burger menu icon
 const Dropdown = () => {
   // Variable and hooks to toggle icons from burger menu icon to closing one
@@ -24,35 +23,38 @@ const Dropdown = () => {
 
   return (
     <div className="relative">
-      <div className="flex justify-start p-6 sticky top-0">
+      <div className="flex justify-start m-4">
         <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
           {/* DropdownMenuTrigger is the "button" that triggers the menu */}
-          <DropdownMenuTrigger asChild className=" bg-[#f2f2f2]/80 h-14 w-14 rounded-full flex items-center justify-center shadow-xl">
+          <DropdownMenuTrigger
+            asChild
+            className=" bg-[#f2f2f2]/10 h-12 w-12 rounded-full flex items-center justify-center shadow-xl"
+          >
             <button
               onClick={handleToggleDropdown}
               className="focus:outline-none "
             >
               {isOpen ? (
                 <div className="">
-                  <div className=" w-7 h-0.5 bg-green_sid transform rotate-45"></div>
-                  <div className=" w-7 h-0.5 bg-pink_sid transform -rotate-45"></div>
+                  <div className=" w-6 h-0.5 bg-green_sid transform rotate-45"></div>
+                  <div className=" w-6 h-0.5 bg-pink_sid transform -rotate-45"></div>
                 </div>
               ) : (
                 <div className="space-y-[0.3rem]">
-                  <span className="block w-7 h-[0.2rem] bg-green_sid"></span>
-                  <span className="block w-5 h-[0.18rem] bg-pink_sid"></span>
                   <span className="block w-6 h-[0.2rem] bg-green_sid"></span>
+                  <span className="block w-4 h-[0.18rem] bg-pink_sid"></span>
+                  <span className="block w-5 h-[0.2rem] bg-green_sid"></span>
                 </div>
               )}
             </button>
           </DropdownMenuTrigger>
           {/* Style DropdownMenuContent to style the whole container */}
-          <DropdownMenuContent className="bg-[#f2f2f2]/90 h-screen w-screen mt-6">
+          <DropdownMenuContent className="bg-[#f2f2f2] h-screen w-screen mt-4 border-none z-40">
             <div className="space-y-10 m-6">
               <div className="flex flex-row m-0 p-0">
                 <DropdownMenuLabel>
                   <Link
-                    className="text-slate-900 font-medium relative text-slate-900 font-medium line-gradient-green"
+                    className="text-lg text-slate-900 font-normal relative  line-gradient-green"
                     href="/"
                   >
                     Home
@@ -63,8 +65,7 @@ const Dropdown = () => {
                 <div className="flex flex-row">
                   <DropdownMenuLabel>
                     <Link
-                      className="text-slate-900 font-medium relative text-slate-900 font-medium line-gradient-pink"
-
+                      className="text-lg text-slate-900 font-normal relative  line-gradient-pink"
                       href="/bespoke-cakes"
                     >
                       Bespoke Cakes
@@ -76,7 +77,7 @@ const Dropdown = () => {
                 <div className="flex flex-row">
                   <DropdownMenuLabel>
                     <Link
-                      className="text-slate-900 font-medium relative text-slate-900 font-medium line-gradient-green"
+                      className="text-lg text-slate-900 font-normal relative  line-gradient-green"
                       href="/tarot-readings"
                     >
                       Tarot Readings
@@ -87,7 +88,7 @@ const Dropdown = () => {
               <div className="flex flex-row">
                 <DropdownMenuLabel>
                   <Link
-                    className="text-slate-900 font-medium relative text-slate-900 font-medium line-gradient-pink"
+                    className="text-lg text-slate-900 font-normal relative   line-gradient-pink"
                     href="/about-us"
                   >
                     About us
@@ -97,7 +98,7 @@ const Dropdown = () => {
               <div className="flex flex-row">
                 <DropdownMenuLabel>
                   <Link
-                    className="text-slate-900 font-medium relative text-slate-900 font-medium line-gradient-green"
+                    className="text-lg text-slate-900 font-normal relative   line-gradient-green"
                     href="/contacts"
                   >
                     Contact
@@ -108,7 +109,7 @@ const Dropdown = () => {
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
-    </div>
+      </div>
   );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,20 +2,13 @@ import React from 'react';
 
 import Image from 'next/image'
 
-// shadcn/ui components
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 const Header = () => {
   return (
     <div className='flex flex-col items-center justify-center'>
-      {/* <Avatar className='w-20 h-20 my-6 border border-slate-700'>
-        <AvatarImage src='logo.png' />
-        <AvatarFallback>21 SID</AvatarFallback>
-      </Avatar>
-      <h1 className='mb-2 text-xl font-medium text-slate-800'>21 SID</h1>*/}
-        <div className="h-auto max-w-full flex justify-center">
+        <div className="h-auto w-full flex justify-center">
         <Image
-            className="h-auto max-w-full rounded-lg"
+            className="h-auto max-w-full"
             src="/logo-removebg.png"
             alt=""
             width={500}
