e8299a870a7d12f360e3a46263303948d3b1e9dc
marcornc <130748589+marcornc@users.noreply.github.com>
2024-06-18 15:54:34 +0100

    fixed typescript for InstaCarousel

--- src/components/InstaCarousel.tsx
+++ src/components/InstaCarousel.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import * as React from "react";
-
 import Image from "next/image";
-
 import axios from "axios";
-
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
@@ -14,18 +11,23 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-
 import Autoplay from "embla-carousel-autoplay";
 
+// Define the Image interface
+interface ImageData {
+  id: string;
+  download_url: string;
+}
+
 export default function InstaCarousel() {
-  const [images, setImages] = React.useState([]);
+  const [images, setImages] = React.useState<ImageData[]>([]);
+
   // Fetch data from the API
   const fetchData = async () => {
     try {
-      const response = await axios.get(
+      const response = await axios.get<ImageData[]>(
         "https://picsum.photos/v2/list?page=1&limit=5"
       );
-      console.log(response.data[0])
       setImages(response.data);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -50,14 +52,14 @@ export default function InstaCarousel() {
       plugins={[plugin.current]}
       onMouseEnter={plugin.current.stop}
       onMouseLeave={plugin.current.reset}
-      className="w-[60%] sm:w-[80%] "
+      className="w-[60%] sm:w-[80%]"
     >
       <CarouselContent className="-ml-1">
         {images.map((image) => (
           <CarouselItem key={image.id} className="pl-1 md:basis-1/2 lg:basis-1/3">
             <div className="p-1">
               <Card className="border-none">
-                <CardContent className="flex items-center justify-center p-6 ">
+                <CardContent className="flex items-center justify-center p-6">
                   <Image
                     className="h-auto max-w-full rounded-xl drop-shadow-xl"
                     src={image.download_url}
@@ -70,18 +72,6 @@ export default function InstaCarousel() {
             </div>
           </CarouselItem>
         ))}
-
-        {/* {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-2xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))} */}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
