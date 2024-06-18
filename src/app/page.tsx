169af261d74218632d25193d5e49fe31132e69f5
mickeymarse <mmserrapica@tutanota.com>
2024-06-18 16:48:30 +0100

    created about us page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-
-import Gallery from "@/components/Gallery";
-import Image from "next/image";
+import Gallery from '@/components/Gallery';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex justify-center py-[50px]">
+    <main className='flex justify-center py-[50px]'>
       <Gallery />
     </main>
   );
-
 }
