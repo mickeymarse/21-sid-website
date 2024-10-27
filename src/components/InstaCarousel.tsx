e2fbdeb458102aae50389d834b2fead0f235dbc3
mickeymarse <mmserrapica@tutanota.com>
2024-10-27 22:17:32 +0000

    Updated InstaCarousel with Instagram API

--- src/components/InstaCarousel.tsx
+++ src/components/InstaCarousel.tsx
@@ -7,18 +7,33 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
+import Link from 'next/link';
 
-interface ImageData {
-  id: string;
-  download_url: string;
+export interface ImageData {
+  data: Data[];
 }
 
-async function getImages() {
-  const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5', {
-    next: {
-      revalidate: 3600,
-    },
-  });
+export interface Data {
+  id: string;
+  caption: string;
+  media_url: string;
+  timestamp: string;
+  media_type: string;
+  permalink: string;
+}
+
+async function getImages(): Promise<ImageData> {
+  const response = await fetch(
+    'https://graph.instagram.com/v20.0/17841407396810214/media?fields=id,caption,media_url,timestamp,media_type,permalink',
+    {
+      next: {
+        revalidate: 3600,
+      },
+      headers: {
+        Authorization: `Bearer ${process.env.INSTAGRAM_TOKEN}`,
+      },
+    }
+  );
 
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
@@ -28,7 +43,8 @@ async function getImages() {
 }
 
 export default async function InstaCarousel() {
-  const images: ImageData[] = await getImages();
+  const imagesData: ImageData = await getImages();
+  const images: Data[] = imagesData.data;
 
   return (
     <Carousel
@@ -36,25 +52,33 @@ export default async function InstaCarousel() {
         align: 'start',
         loop: true,
       }}
-      className='w-[60%] sm:w-[80%]'
+      className='w-[60%] sm:w-[80%] mt-10'
     >
       <CarouselContent className='-ml-1'>
-        {images.map((image) => (
-          <CarouselItem key={image.id} className='pl-1 md:basis-1/2 lg:basis-1/3'>
-            <Card>
-              <CardContent className='flex items-center justify-center m-6'>
-                <Image
-                  className='h-auto max-w-full rounded-xl'
-                  src={image.download_url}
-                  alt='Carousel image'
-                  width={500}
-                  height={500}
-                  priority={true}
-                />
-              </CardContent>
-            </Card>
-          </CarouselItem>
-        ))}
+        {images.slice(0, 15).map(
+          (image) =>
+            image.media_type === 'IMAGE' && (
+              <CarouselItem key={image.id} className='pl-1 md:basis-1/2 lg:basis-1/3'>
+                <Card>
+                  <CardContent className='flex items-center justify-center m-6'>
+                    <Link href={image.permalink} target='_blank' className='relative group'>
+                      <Image
+                        className='h-auto max-w-full rounded-xl transition-opacity duration-300 group-hover:opacity-50'
+                        src={image.media_url}
+                        alt='Carousel image'
+                        width={350}
+                        height={350}
+                        priority={true}
+                      />
+                      <p className='absolute inset-0 flex items-center justify-center text-white text-center font-bold text-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100 bg-black bg-opacity-50 rounded-xl px-5'>
+                        {image.caption.substring(0, 250) + '...'}
+                      </p>
+                    </Link>
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            )
+        )}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
