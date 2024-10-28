870c8598d8e5b661c492c143727775b4b7e0d353
mickeymarse <mmserrapica@tutanota.com>
2024-10-28 23:09:32 +0000

    Added video/reel rendering to InstaCarousel

--- src/components/InstaCarousel.tsx
+++ src/components/InstaCarousel.tsx
@@ -52,33 +52,44 @@ export default async function InstaCarousel() {
         align: 'start',
         loop: true,
       }}
-      className='w-[60%] sm:w-[80%] mt-10'
+      className='w-[60%] sm:w-[80%] mt-20'
     >
       <CarouselContent className='-ml-1'>
-        {images.slice(0, 15).map(
-          (image) =>
-            image.media_type === 'IMAGE' && (
-              <CarouselItem key={image.id} className='pl-1 md:basis-1/2 lg:basis-1/3'>
-                <Card>
-                  <CardContent className='flex items-center justify-center m-6'>
-                    <Link href={image.permalink} target='_blank' className='relative group'>
+        {images.slice(0, 9).map((image) => (
+          <CarouselItem key={image.id} className='pl-1 md:basis-1/2 lg:basis-1/3'>
+            <Card>
+              <CardContent className='flex items-center justify-center m-6'>
+                <Link href={image.permalink} target='_blank' className='relative group'>
+                  {image.media_type !== 'VIDEO' ? (
+                    <article className='relative w-[300px] h-[300px] overflow-clip'>
                       <Image
-                        className='h-auto max-w-full rounded-xl transition-opacity duration-300 group-hover:opacity-50'
+                        className='rounded-xl transition-opacity duration-300 group-hover:opacity-50'
                         src={image.media_url}
-                        alt='Carousel image'
-                        width={350}
-                        height={350}
+                        alt='Carousel image from 21 Sid Instagram page'
+                        width='300'
+                        height='300'
                         priority={true}
                       />
-                      <p className='absolute inset-0 flex items-center justify-center text-white text-center font-bold text-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100 bg-black bg-opacity-50 rounded-xl px-5'>
-                        {image.caption.substring(0, 250) + '...'}
-                      </p>
-                    </Link>
-                  </CardContent>
-                </Card>
-              </CarouselItem>
-            )
-        )}
+                    </article>
+                  ) : (
+                    <article className='relative w-[300px] h-[300px] overflow-clip'>
+                      <video
+                        src={image.media_url}
+                        width='300'
+                        height='300'
+                        title='Carousel image from 21 Sid Instagram page'
+                        className='rounded-xl transition-opacity duration-300 group-hover:opacity-50'
+                      ></video>
+                    </article>
+                  )}
+                  <p className='absolute inset-0 flex items-center justify-center text-white text-center font-bold text-lg opacity-0 transition-opacity duration-300 group-hover:opacity-100 bg-black bg-opacity-50 rounded-xl px-5'>
+                    {image.caption.substring(0, 150) + '...'}
+                  </p>
+                </Link>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
