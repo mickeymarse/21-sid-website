37f940d4df298c6aeccdb0674c53129d33d49d66
marcornc <130748589+marcornc@users.noreply.github.com>
2024-11-27 21:06:28 +0000

    fixed typescript error in gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import { defineQuery } from 'next-sanity';
 import { sanityFetch } from '@/sanity/live';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { client } from '@/sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 import Image from 'next/image';
+import { Key } from 'react';
 
 const PICTURES_QUERY = defineQuery(`*[
   _type == "picture"
@@ -20,20 +22,19 @@ export default async function Gallery() {
   const { data: pictures } = await sanityFetch({ query: PICTURES_QUERY });
 
   return (
+    <ul className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+      {pictures.map((picture: any) => (
+        <li className="p-4 rounded-lg" key={picture._id}>
 
-      <ul className="grid grid-cols-1 gap-4 lg:grid-cols-3">
-        {pictures.map((picture) => (
-          <li className="p-4 rounded-lg" key={picture._id}>
-
-                <Image
-                  src={urlFor(picture.image).url() || 'https://via.placeholder.com/550x310'} // Use a generic placeholder
-                  alt={picture.name || 'Picture'}
-                  width='850'
-                  height='610'
-                  className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full"
-                />
-          </li>
-        ))}
-      </ul>
+              <Image
+                src={urlFor(picture.image)?.url() || 'https://via.placeholder.com/550x310'}
+                alt={picture.name || 'Picture'}
+                width={850}
+                height={610}
+                className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full"
+              />
+        </li>
+      ))}
+    </ul>
   );
 }
